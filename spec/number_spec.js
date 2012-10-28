6689771753276f731786ff58311179416291e055
joerodriguez <joerodriguez@gmail.com>
2012-10-28 13:43:56 -0300

    Update spec/number_spec.js
    clean number_spec example var names

--- spec/number_spec.js
+++ spec/number_spec.js
@@ -2,8 +2,8 @@ suite("NumberSuite", function(){
   it("should do cool stuff n times", function(){
     var a = [];
     var n = 3;
-    var result = n.times(function(n){
-      a.push(n);
+    var result = n.times(function(num){
+      a.push(num);
     });
     assert_arrays_equal(a,[0,1,2]);
     assert_equal(result,3);
